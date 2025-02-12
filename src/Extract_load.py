d1ff0ad0f39d5d8707961fecf454af846ee7215d
Maxwilliam <maxwilliamgomess@gmail.com>
2025-02-11 23:21:47 -0300

    Criado as .env e avançando no Load

--- src/Extract_load.py
+++ src/Extract_load.py
@@ -2,14 +2,32 @@
 import requests
 import os
 import pandas as pd
-import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import glob
+
 
 # import das minhas variáveis de ambiente
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 
 
 
 # Fazer o webscraping dos arquivos
 
+
+## Função para baixar arquivos
 def baixar_arquivo(url, endereco):
  """
     Fazendo o download dos arquivos e salvando no endereço especificado.
@@ -22,19 +40,25 @@ def baixar_arquivo(url, endereco):
  else:
    resposta.raise_for_status()
 
+  
+## Função para verificar se um arquivo já existe
 def verificar_arquivo_existe(endereco):
     """
     Verifica se um arquivo já existe no diretório.
     """
     return os.path.exists(endereco)
 
+
+
 if __name__ == "__main__":
   BASE_URL = "https://siros.anac.gov.br/siros/registros/diversos/vra/2024/VRA_2024_{:02d}.csv"
   DATASET_DIR = 'dataset'
+  TABELA = DB_NAME
 
   ## Criando diretório caso não exista
   os.makedirs(DATASET_DIR, exist_ok=True)
 
+# Faz o download dos arquivos
   for i in range(1 , 13):
    nome_arquivo = os.path.join(DATASET_DIR, f'VRA_2024_{i:02d}.csv')
 
@@ -47,9 +71,32 @@ if __name__ == "__main__":
    baixar_arquivo(BASE_URL.format(i), nome_arquivo)
 
 
-# Carregando os arquivos
+
+# Caminho para os arquivos CSV
+caminho = "C:/Users/PICHAU/Documents/Analise_de_Pontualidade_Performance_Anac/dataset/"
+
+# Listar todos os arquivos CSV no diretório que começam com "VRA_2024"
+arquivos = glob.glob(caminho + "VRA_2024_*.csv")
+
+# Carregar e concatenar os arquivos em um único DataFrame
+dataframe = pd.concat((pd.read_csv(f, delimiter=';') for f in arquivos), ignore_index=True)
+    
+    
+    
+# Carrega os dados no banco de dados, usando o schema especificado
+    
+def salvar_no_postgres(df, schema='public'):
+    
+  df.to_sql(
+              TABELA,
+              engine,  
+              if_exists="append",
+              index=True,
+              schema = schema)
 
 
-
-# Armazenando os dados
-
+#Carrega os arquivos CSV no banco de dados
+salvar_no_postgres(
+        dataframe,
+        schema=DB_SCHEMA
+    )
